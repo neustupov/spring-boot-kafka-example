04fc78dbd22348a83777c28ed70e6e72b77fca52
neustupov <neustupov@yandex.ru>
2020-01-07 15:34:34 +0300

    Initial commit

---
+++ src/test/java/ru/neustupov/springbootkafkaexample/SpringBootKafkaExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.neustupov.springbootkafkaexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootKafkaExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
