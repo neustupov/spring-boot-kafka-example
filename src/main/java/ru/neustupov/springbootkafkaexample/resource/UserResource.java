d90466c5f2f8756d7ee32b20a0eae95bfda934d1
neustupov <neustupov@yandex.ru>
2020-01-08 17:26:03 +0300

    added config and model

--- src/main/java/ru/neustupov/springbootkafkaexample/resource/UserResource.java
+++ src/main/java/ru/neustupov/springbootkafkaexample/resource/UserResource.java
@@ -6,19 +6,25 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ru.neustupov.springbootkafkaexample.model.User;
 
 @RestController
 @RequestMapping("kafka")
 public class UserResource {
 
+  private KafkaTemplate<String, User> kafkaTemplate;
+
   @Autowired
-  KafkaTemplate<String, String> kafkaTemplate;
+  public UserResource(KafkaTemplate<String, User> kafkaTemplate) {
+    this.kafkaTemplate = kafkaTemplate;
+  }
+
   private static final String TOPIC = "Kafka_Example";
 
-  @GetMapping("/publish/{message}")
-  public String post(@PathVariable("message") final String message){
+  @GetMapping("/publish/{name}")
+  public String post(@PathVariable("name") final String name){
 
-    kafkaTemplate.send(TOPIC, message);
+    kafkaTemplate.send(TOPIC, new User(name, "Systematic", 12000L));
 
     return "Published successfully";
   }
